8363c4b74e76590088b6b79b77f6aea96dc33080
BlueManedHawk <bluemanedhawk@gmail.com>
2022-05-07 02:34:59 -0400

    Program skeleton in place
    Sorry I haven't been working on this very much recently.  Anyway, this
    commit sets up the skeleton of the program.  Hopefully, i'll be able to
    get this program done tomorrow.

--- Source/Main.c
+++ Source/Main.c
@@ -1,5 +1,4 @@
 /* LICENSE
- *
  * Copyright © 2022 Blue-Maned_Hawk.  All rights reserved.
  *
  * This project should have come with a file called `LICENSE`.  In the event of any conflict between this comment and that file, that file shall be considered the authority.
@@ -15,12 +14,78 @@
 #include <unistd.h>
 #include "SDL.h"
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include "Menu.h"
+#include "Questions.h"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] const char ** argv){
+#define ever ;;
+
+enum state_t {
+	menu,
+	questions
+};
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] const char ** argv)
+{
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 	atexit(SDL_Quit);
+
 	SDL_Window * window = SDL_CreateWindow("ëšho'hlorẓûţc hwomùaržrıtéu-erţtenļıls", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-	sleep(5);
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_Event e;
+	bool quit = false;
+	enum state_t state = menu;
+	struct timespec firsttime, secondtime;
+	for (ever){
+		timespec_get(&firsttime, TIME_UTC);
+
+		while (SDL_PollEvent(&e)){
+			if (e.type == SDL_QUIT){
+				quit++;
+				break;
+			} else if (state == menu) {
+				menu_handle_events(e);
+			} else if (state == questions) {
+				questions_handle_events(e);
+			} else {
+				printf("\033[31mThe program has entered an unknown state and will now exit.  Apologies for the inconvenience.");
+				exit(EXIT_FAILURE);
+			}
+		}
+		if (quit)
+			break;
+
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
+		SDL_RenderClear(renderer);
+
+		switch (state){
+		case menu:
+			menu_render(renderer);
+			break;
+		case questions:
+			questions_render(renderer);
+			break;
+		default:
+			printf("\033[31mThe program has entered an unknown state and will now exit.  Apologies for the inconvenience.");
+			exit(EXIT_FAILURE);
+		}
+
+		SDL_RenderPresent(renderer);
+
+		timespec_get(&secondtime, TIME_UTC);
+		secondtime.tv_nsec -= firsttime.tv_nsec; secondtime.tv_sec -= firsttime.tv_sec;
+		if (secondtime.tv_sec > 0)
+			continue;
+		if (secondtime.tv_nsec > 27000000)
+			continue;
+		firsttime.tv_sec = 0;
+		firsttime.tv_nsec = 27000000 - secondtime.tv_nsec;
+		nanosleep(&firsttime, NULL);
+	}
+
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_QuitSubSystem(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 	SDL_Quit();
